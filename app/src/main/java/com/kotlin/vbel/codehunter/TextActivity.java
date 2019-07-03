2627efa6f7ac366f9ec675c2176ce1b28b37c963
Vladislav Belovitsky <vbelovitsky@yandex.ru>
2019-07-03 16:02:56 +0300

    text activity design and logic

--- app/src/main/java/com/kotlin/vbel/codehunter/TextActivity.java
+++ app/src/main/java/com/kotlin/vbel/codehunter/TextActivity.java
@@ -1,8 +1,14 @@
 package com.kotlin.vbel.codehunter;
 
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TextActivity extends AppCompatActivity {
 
@@ -13,8 +19,32 @@ public class TextActivity extends AppCompatActivity {
 
         TextView recognizedTextView = findViewById(R.id.recognizedText);
         String recognizedText = getIntent().getStringExtra("recognizedText");
-
         recognizedTextView.setText(recognizedText);
+
+        String[] languages_data = getResources().getStringArray(R.array.languages);
+        int len = languages_data.length;
+        String[] languages = new String[len];
+        String[] expansions = new String[len];
+
+        for (int i = 0; i < len; i++){
+            String[] data = languages_data[i].split("=");
+            languages[i] = data[0];
+            expansions[i] = data[1];
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, languages);
+        AutoCompleteTextView actv = findViewById(R.id.autoLanguage);
+        actv.setAdapter(adapter);
+
+
+        ImageButton copyButton = findViewById(R.id.imageButtonCopy);
+        ImageButton saveButton = findViewById(R.id.imageButtonSave);
+        ImageButton sendButton = findViewById(R.id.imageButtonSend);
+
+        
+
+
+
 
     }
 }
